1062f68f02d48b710af232065e747ab2e67b32ad
MTRecouso <mateus.recouso@hotmail.com>
2020-03-11 00:33:46 -0300

    Moved monad tests for Identity to the tests file

--- Identity/identity.js
+++ Identity/identity.js
@@ -22,28 +22,6 @@ const Identity = (value) => ({
 
 module.exports = Identity
 
-/*
-Proving that it satisfies the monadic laws:
-
-Left identity: return a >>= f ≡ f a
-
-Right identity: m >>= return ≡ m
-
-Associativity: (m >>= f) >>= g ≡ m >>= (\x -> f x >>= g) 
-*/
-
-const leftId = (f,a) => Identity().mReturn(a).mBind(f).valueOf() === f(a).valueOf()
-
-const rightId = (m) => m.mBind(m.mReturn).valueOf() === m.valueOf();
-
-const associativity = (m,f,g) => m.mBind(f).mBind(g).valueOf() ===  m.mBind((x) => f(x).mBind(g)).valueOf();
-
-console.log('left identity', leftId(add2,2))
-
-console.log('right identity', rightId(Identity(2)));
-
-console.log('associativity', associativity(Identity(2),add2,multiply2));
-
 
 
 
--- Identity/identity.test.js
+++ Identity/identity.test.js
@@ -1,5 +1,5 @@
 const Identity = require("./identity");
-const { compose } = require('../common_functions');
+const { compose } = require("../common_functions");
 
 describe("Semigroup implementation for Identity", () => {
   test("Mappend of two Identity containing numbers sums the two and return an Identity with the result", () => {
@@ -24,28 +24,27 @@ describe("Semigroup implementation for Identity", () => {
 });
 
 describe("Monoid implementation for Identity", () => {
+  const x = Identity(2);
   test("Right identity rule for Monoids. x <> mempty = x", () => {
-    const x = Identity(2);
     const mempty = Identity().mempty();
     expect(x.mappend(mempty).valueOf()).toBe(x.valueOf());
   });
 
   test("Left identity rule for Monoids. mempty <> x = x", () => {
-    const x = Identity(2);
     const mempty = Identity().mempty();
     expect(mempty.mappend(x).valueOf()).toBe(x.valueOf());
   });
 });
 
 describe("Functor implementation for Identity", () => {
+  const fa = Identity(2);
+
   test("Identity rule for Functors. fmap id = id", () => {
-    const fa = Identity(2);
     const identityFn = a => a;
     expect(fa.fmap(identityFn).valueOf()).toBe(fa.valueOf());
   });
 
   test("Composition rule for Functors. fmap f . g = fmap f . fmap g", () => {
-    const fa = Identity(2);
     const add5 = a => a + 5;
     const add3 = a => a + 3;
     expect(fa.fmap(compose(add3, add5)).valueOf()).toBe(
@@ -54,5 +53,35 @@ describe("Functor implementation for Identity", () => {
         .fmap(add5)
         .valueOf()
     );
+  });
+});
+
+describe("Monad implementation for Identity", () => {
+  const f = a => Identity(a + 2);
+  const m = Identity(2);
+
+  test("Left identity rule for Monads. return a >>= f ≡ f a", () => {
+    const a = 2;
+    expect(
+      Identity()
+        .mReturn(a)
+        .mBind(f)
+        .valueOf()
+    ).toBe(f(a).valueOf());
+  });
+
+  test("Right identity rule for Monads. m >>= return ≡ m", () => {
+    const m = Identity(2);
+    expect(m.mBind(m.mReturn).valueOf()).toBe(m.valueOf());
+  });
+
+  test("Associatitity identity rule for Monads. (m >>= f) >>= g ≡ m >>= (\\x -> f x >>= g)", () => {
+    const g = a => Identity(a * 2);
+    expect(
+      m
+        .mBind(f)
+        .mBind(g)
+        .valueOf()
+    ).toBe(m.mBind(x => f(x).mBind(g)).valueOf());
   });
 });
