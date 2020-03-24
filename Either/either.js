5387d28da6acf995891ab64098a9dc722e801fce
MTRecouso <mateus.recouso@hotmail.com>
2020-03-23 23:40:33 -0300

    Removed unecessary whitespaces from Either

--- Either/either.js
+++ Either/either.js
@@ -3,15 +3,10 @@
 const Either = {
     Right: (value) => ({
         valueOf: () => value,
-
         //Making Right an instance of Semigroup
         mappend: mb => Either.Right(value),
-
         //Making Right an instance of Functor
         fmap: fn => Either.Right(fn(value)), 
-
-
-    
         //Making Right an instance of Monad 
         mBind: fn => fn(value),
         getType: () => "Right"
@@ -19,16 +14,12 @@ const Either = {
 
     Left: (value) => ({
         valueOf: () => value,
-
         //Making Right an instance of Semigroup
         mappend: mb => mb,
-  
         //Making Left an instance of Functor
         fmap: _ => Either.Left(value),  
-
         //Making Left an instance of Monad 
         mBind: _ => Either.Left(value),  
-
         getType: () => "Left"
     }),
     //Making Either an instance of Applicatives
